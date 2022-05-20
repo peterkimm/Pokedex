474c52e600e29f9cca6cf1b34d3a18ef44f385fa
Peter <peterkimm35@gmail.com>
2022-05-19 22:51:55 -0700

    create and delete

--- server.js
+++ server.js
@@ -17,39 +17,39 @@ app.get('/pokedex', (req, res) => {
     });
 });
 
-
-
-
 // NEW
-
-
-
-
+app.get('/pokedex/new', (req, res) => {
+    res.render('new.ejs', {
+        data: pokedex
+    })
+  });
 
 // DELETE
-
-
-
-
+app.delete("/pokedex/:indexOfPokedexArray", (req, res) => {
+    pokedex.splice(req.params.indexOfPokedexArray, 1) 
+    res.redirect("/pokedex")
+  });
 
 // UPDATE
-
-
-
-
+app.put("/pokedex/:indexOfPokedexArray", (req, res) => {
+    pokedex[req.params.indexOfPokedexArray] = req.body 
+    res.redirect("/pokedex")
+  });
 
 // CREATE
-
-
-
-
+app.post("/pokedex", (req, res) => {
+    pokedex.unshift(req.body)
+    res.redirect("/pokedex") 
+  });
 
 // EDIT
-
-
-
-
-
+app.get("/pokedex/:indexOfPokedexArray/edit", (req, res) => {
+    res.render(
+      "edit.ejs", {
+        data: pokedex[req.params.indexOfPokedexArray],
+        index: req.params.indexOfPokedexArray, 
+      });
+  });
 
 // SHOW
 app.get('/pokedex/:id', (req, res) => {
